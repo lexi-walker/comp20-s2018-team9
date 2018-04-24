d54a847f2acfc4b5df48de04fb177b2607f5d0b3
Nimish Adhikari <nimishadhikari@gmail.com>
2018-04-24 13:41:13 -0400

    adding data in json for calendar

--- calendar-script.js
+++ calendar-script.js
@@ -72,6 +72,12 @@ function listUpcomingEvents() {
                           if (events.length > 0) {
                                     for (i = 0; i < events.length; i++) {
                                               var event = events[i];
+                                              var eventdata = {
+                                                      "time": event.start.dateTime,
+                                                      "summary": event.summary,
+                                                      "location": event.location
+                                              };
+                                              console.log(eventdata);
                                               var when = event.start.dateTime;
                                               if (!when) {
                                                       when = event.start.date;
