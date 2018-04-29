58bf1de4ddcb486e0f2585c1456fa2eebc748fb8
Nimish Adhikari <nimishadhikari@gmail.com>
2018-04-29 10:54:47 -0400

    adding changes to server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
+var cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
-}).configure(function() {
-    app.use('/images', express.static(__dirname + '/images'));
-}).listen(port);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoUri = process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/ridesdata';
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+	db = databaseConnection;
+});
+
+app.post('/storedata', function(request, response) {
+        if ( request.body.username != undefined && request.body.time != undefined && request.body.endtime != undefined && request.body.summary != undefined && request.body.location != undefined) {
+                
+        }
+});
