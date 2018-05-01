7168f886819c71b422e021a7e12fe71748f5da57
Nimish Adhikari <nimishadhikari@gmail.com>
2018-05-01 11:09:36 -0400

    old map script

--- map-script.js
+++ map-script.js
@@ -1,6 +1,3 @@
-var decoded = [];
-
-
 function initMap(){
 
   lifemap = new google.maps.Map(document.getElementById('map'), {
@@ -17,8 +14,29 @@ var delay = 50;
 function plotNext(iter, locations) {
   geocoder.geocode({'address':locations[iter].place}, function (results,status){
     if (status == google.maps.GeocoderStatus.OK) {
+      var pos = results[0].geometry.location;
 
-      decoded.push(results[0].geometry.location);
+      heatmap = new google.maps.visualization.HeatmapLayer({
+        data: [{location: new google.maps.LatLng(pos.lat(), pos.lng()), weight:locations[iter].time}],
+        map: lifemap,
+        radius: 20,
+        gradient: [
+                  'rgba(255, 255, 255, 0)',
+                  'rgba(193, 235, 176, 1)',
+                  'rgba(144, 177, 144, 1)',
+                  'rgba(33, 109, 185, 1)',
+                  'rgba(0, 63, 255, 1)',
+                  'rgba(0, 0, 255, 1)',
+                  'rgba(0, 0, 223, 1)',
+                  'rgba(0, 0, 191, 1)',
+                  'rgba(0, 0, 159, 1)',
+                  'rgba(0, 0, 127, 1)',
+                  'rgba(63, 0, 91, 1)',
+                  'rgba(127, 0, 63, 1)',
+                  'rgba(199, 20, 109, 1)',
+                  'rgba(255, 0, 147, 1)'
+                ]
+      });
   /*
         var marker = new google.maps.Marker({
           map: lifemap,
@@ -38,34 +56,8 @@ function recurse(iter, locations) {
   if (iter < locations.length) {
     setTimeout('plotNext("'+iter+'",locations)', delay);
   }
-
-  
-
 }
 
 function getCoords(locations){
-  decoded = [];
   recurse(0, locations);
-
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: decoded,//[{location: new google.maps.LatLng(pos.lat(), pos.lng()), weight:locations[iter].time}]
-    map: lifemap,
-    radius: 25,
-    gradient: [
-      'rgba(255, 255, 255, 0)',
-      'rgba(193, 235, 176, 1)',
-      'rgba(144, 177, 144, 1)',
-      'rgba(33, 109, 185, 1)',
-      'rgba(0, 63, 255, 1)',
-      'rgba(0, 0, 255, 1)',
-      'rgba(0, 0, 223, 1)',
-      'rgba(0, 0, 191, 1)',
-      'rgba(0, 0, 159, 1)',
-      'rgba(0, 0, 127, 1)',
-      'rgba(63, 0, 91, 1)',
-      'rgba(127, 0, 63, 1)',
-      'rgba(199, 20, 109, 1)',
-      'rgba(255, 0, 147, 1)'
-    ]
-  });
 }
